f84527f3977d46fedf80f2da287761aa2e76db18
Justin Holmes <justinmichaelholmes@gmail.com>
2016-03-12 21:55:42 -0500

    adding context root suffix for all apps

--- startJBossWithOptions.sh
+++ startJBossWithOptions.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
 
-JBOSS_WEB_PATH=/opt/jboss/jboss-eap-6.4/standalone/deployments/business-central.war/WEB-INF/jboss-web.xml
+JBOSS_WEB_PATH_1=/opt/jboss/jboss-eap-6.4/standalone/deployments/business-central.war/WEB-INF/jboss-web.xml
+JBOSS_WEB_PATH_2=/opt/jboss/jboss-eap-6.4/standalone/deployments/kie-server.war/WEB-INF/jboss-web.xml
+JBOSS_WEB_PATH_3=/opt/jboss/jboss-eap-6.4/standalone/deployments/dashbuilder.war/WEB-INF/jboss-web.xml
+
 JBOSS_START_SCRIPT=/opt/jboss/jboss-eap-6.4/bin/standalone.sh
 
 echo -e "\n\n================================================\n"
 
-if [ -z ${BC_CONTEXT_ROOT+x} ];
+if [ -z ${CONTEXT_ROOT_SUFFIX+x} ];
 	then
-		echo "BC_CONTEXT_ROOT is unset. Using default business-central URL context root."
+		echo "CONTEXT_ROOT_SUFFIX is unset. Using default business-central URL context root."
  	else
- 		echo "BC_CONTEXT_ROOT is set. The URL context root will be '$BC_CONTEXT_ROOT'"
- 		sed -i "s/<\/security-domain>/<\/security-domain>\n\t<context-root>$BC_CONTEXT_ROOT<\/context-root>/" $JBOSS_WEB_PATH
+ 		echo "CONTEXT_ROOT_SUFFIX is set. The URL context root will be '$CONTEXT_ROOT_SUFFIX'"
+ 		sed -i "s/<\/security-domain>/<\/security-domain>\n\t<context-root>business-central-$CONTEXT_ROOT_SUFFIX<\/context-root>/" $JBOSS_WEB_PATH_1
+ 		sed -i "s/<\/security-domain>/<\/security-domain>\n\t<context-root>kie-server-$CONTEXT_ROOT_SUFFIX<\/context-root>/" $JBOSS_WEB_PATH_2
+ 		sed -i "s/<\/security-domain>/<\/security-domain>\n\t<context-root>dashbuilder-$CONTEXT_ROOT_SUFFIX<\/context-root>/" $JBOSS_WEB_PATH_3
 fi
 
 echo -e "\n===================================================\n\n"
